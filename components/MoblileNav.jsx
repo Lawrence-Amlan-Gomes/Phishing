d8327388a24bb94e7e38e62f48c1bb457abfeb01
Lawrence-Amlan-Gomes <amlangomes@gmail.com>
2024-12-14 17:20:17 +0600

    again alined

--- components/MoblileNav.jsx
+++ components/MoblileNav.jsx
@@ -3,15 +3,13 @@ import NavItems from "./NavItems";
 export default function MobileNav() {
   return (
     <div className="w-full h-full text-[#eeeeee] border-[#222222] border-t-[1px] bg-[#080808] relative text-[10px] md:text-[12px] lg:text-[14px] xl:text-[18px] 2xl:text-[22px] justify-center flex items-center">
-      <div className="w-full relative h-full">
-        <div className="flex items-center justify-center w-full relative h-full visible sm:hidden text-[10px]">
+        <div className="flex items-center justify-center w-full relative h-full  text-[10px]">
           <NavItems linkTo="/project" text="Projects" />
           <NavItems linkTo="/about" text="About" />
           <NavItems linkTo="/skill" text="Skill" />
           <NavItems linkTo="/education" text="Education" />
           <NavItems linkTo="/contact" text="Contact" />
         </div>
-      </div>
     </div>
   );
 }
